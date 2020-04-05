b4428e1dbbb7daab300be7a46a80565b0b8726fc
akashkumar20 <akash@iofactory.in>
2020-04-05 13:28:23 +0530

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
+const https = (process.env.HTTPS === 'true')
+    ? {
+        http2: true,
+        https: {
+            key: fs.readFileSync(path.join(__dirname, '.ssl/server.key')),
+            cert: fs.readFileSync(path.join(__dirname, '.ssl/server.crt')),
+            ca: fs.readFileSync(path.join(__dirname, '.ssl/rootCA.pem')),
+        },
+    }
+    : {};
 module.exports = {
     mode: 'development',
     entry: {
@@ -48,6 +59,12 @@ module.exports = {
                 ],
             },
             {
+                test:  /\.(png|jpe?g|gif)$/i,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
                 enforce: 'pre',
                 test: /\.js$/,
                 loader: 'source-map-loader',
@@ -61,12 +78,14 @@ module.exports = {
         historyApiFallback: {
             index: 'index.html'
         },
+        publicPath: 'https://gauntlet.askey.in/',
         host: '0.0.0.0',
         hot: false,
         inline: false,
         liveReload: false,
-        port: process.env.PORT,
-        // public: process.env.PUBLIC_URL,
+        port: 3003,
+        public: process.env.ASKEY_WEB_URL,
+        ...https,
     },
     plugins: [
         new Dotenv(),
